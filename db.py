014e1048d968f0f8d437a153222659f9dec2058d
Anupa Alwis <isurajithalwis@gmail.com>
2026-04-20 20:13:06 +0530

    LLM initialized

---
+++ db.py
@@ -0,0 +1,23 @@
+from langchain.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain_ollama import ChatOllama
+from config import DB_URI
+
+def get_database():
+    return SQLDatabase.from_uri(DB_URI)
+
+def get_sql_chain():
+    db = get_database()
+
+    llm = ChatOllama(
+        model="llama3",
+        temperature=0
+    )
+
+    db_chain = SQLDatabaseChain.from_llm(
+        llm=llm,
+        db=db,
+        verbose=True
+    )
+
+    return db_chain
